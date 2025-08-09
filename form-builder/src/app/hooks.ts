6ce7b242345faf20b734ac827b991216569c12df
sarthak singh <buddysuccess7@gmail.com>
2025-08-09 14:44:15 +0530

    fix: adjust import statement in hooks.ts to remove type keyword

--- form-builder/src/app/hooks.ts
+++ form-builder/src/app/hooks.ts
@@ -1,5 +1,5 @@
 // hooks.ts
-import type { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
